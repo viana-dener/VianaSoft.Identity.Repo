09b91040ecc737df963f19e05d0cbf3ff432b2ff
VianaSoft Ltda <vianasoft@outlook.com>
2023-07-27 14:55:37 -0300

    Criando projeto Domain

---
+++ src/VianaSoft.Identity.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VianaSoft.Identity.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string UrlImage { get; set; }
+    }
+}
---
+++ src/VianaSoft.Identity.Domain/Interfaces/IIdentityService.cs
@@ -0,0 +1,17 @@
+﻿using VianaSoft.BuildingBlocks.Core.User.Dto.Request;
+using VianaSoft.BuildingBlocks.Core.User.Dto.Response;
+
+namespace VianaSoft.Identity.Domain.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<ProfileResponseDto> GetProfileAsync();
+        Task<UserLoginResponseDto> LoginAsync(UserLoginRequestDto request);
+        Task<UserLoginResponseDto> RegisterUserAsync(UserRegistrationRequestDto request);
+        Task<bool> ForgotPasswordAsync(ForgotPasswordRequestDto request);
+        Task<bool> ResetPasswordAsync(ResetPasswordRequestDto request);
+        Task<bool> ChangePasswordAsync(ChangePasswordRequestDto request);
+        Task<bool> IsResetPasswordTokenValidAsync(string email, string token);
+        Task LogoutAsync();
+    }
+}
